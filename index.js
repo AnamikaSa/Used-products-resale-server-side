81720bf3079f46c58f5cf9bda75791da1d7c796e
AnamikaSa <amanika0061@gmail.com>
2022-11-25 12:21:10 +0600

    data loaded

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.bu444bw.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const categoriesCollection = client.db('resale_products').collection('categories');
+
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const o = await categoriesCollection.find(query).toArray();
+            res.send(o);
+        })
+
+    }
+    finally { }
+}
+run().catch(console.log);
+
+
+
+app.get('/', async (req, res) => {
+    res.send("Server Running")
+})
+
+app.listen(port, () => console.log(`Running on: ${port}`))
