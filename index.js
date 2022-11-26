14e8195133c8e7855af0a7b20551a3f6550658e1
AnamikaSa <amanika0061@gmail.com>
2022-11-27 03:02:38 +0600

    get datat error solved

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function run() {
             res.send(o);
         })
 
-        app.get('/category/:id',async (req,res)=>{
+        app.get('/categories/:id',async (req,res)=>{
             const id= req.params.id;
             const query= {_id : ObjectId(id) };
             const category = await categoriesCollection.findOne(query);
